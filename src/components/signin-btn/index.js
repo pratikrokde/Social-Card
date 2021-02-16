e1f65e731717b76d7af801af33c3cff67b92cf39
pratikrokde <rokdepratik3@gmail.com>
2021-02-16 13:21:37 +0530

    Initial commit

---
+++ src/components/signin-btn/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+function signInBtn() {
+  return (
+    <div className="signInBtn">
+      <button>Sign In with google</button>
+    </div>
+  );
+}
+
+export default signInBtn;
